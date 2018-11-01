a84306988912c35d851a640994c5d97e5a6b9909
Nathan Thompson <nthompson@thebog.net>
2018-11-01 08:35:19 -0400

    OK. Set the fscking variable, then use it to set the env variable.

--- tools/update.sh
+++ tools/update.sh
@@ -4,7 +4,8 @@ cd ~/dotfiles
 git pull --rebase --stat origin master
 
 if [[ -f ~/.oh-my-zsh/tools/upgrade.sh ]]; then
-	env ZSH=/home/nate/.oh-my-zsh sh $ZSH/tools/upgrade.sh
+	ZSH=/home/nate/.oh-my-zsh
+	env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
 fi
 
 ~/dotfiles/tools/makesymlinks.sh
