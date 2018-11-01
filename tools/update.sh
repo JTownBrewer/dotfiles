49985aab907451403c66fb99adb43a2c34aedb8f
Nathan Thompson <nthompson@thebog.net>
2018-11-01 01:55:00 -0400

    Sigh

--- tools/update.sh
+++ tools/update.sh
@@ -4,7 +4,7 @@ cd ~/dotfiles
 git pull --rebase --stat origin master
 
 if [[ -f ~/.oh-my-zsh/tools/upgrade.sh ]]; then
-	env ZSH=$ZSH sh ${ZSH}/tools/upgrade.sh
+	env ZSH=$ZSH sh {$ZSH}/tools/upgrade.sh
 fi
 
 ~/dotfiles/tools/makesymlinks.sh
