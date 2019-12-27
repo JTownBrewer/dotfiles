f357a1064fa2f32fea924c9bccaac2df7e13f315
Nathan Thompson <nthompson@thebog.net>
2019-12-27 14:27:45 -0500

    Fix home directory to always be set for the current user (not "nate"), Make tmux look for powerline in site-packages (dynamic) Set vim to use expandtabs

--- tools/makesymlinks.sh
+++ tools/makesymlinks.sh
@@ -12,74 +12,74 @@ backupdir=$dir/backup
 ########## create backup
 echo -n "Backup folder [$backupdir]: ... "
 if [[ ! -d $backupdir ]]; then
-	mkdir -p $backupdir 2>&1 > /dev/null
-	echo "Created"
+    mkdir -p $backupdir 2>&1 > /dev/null
+    echo "Created"
 else
-	echo "Exists"
+    echo "Exists"
 fi
 
 dest_dir=${HOME}
 pushd ${HOME} > /dev/null
 
 create_links() {
-	folder=$1
-	# echo "*****"
-	# echo "Config Folder: ${folder}"
-	# echo "Current Directory: $(pwd)"
-	# echo "Directory Stack: [$(dirs)]"
-	# echo "*****"
+    folder=$1
+    # echo "*****"
+    # echo "Config Folder: ${folder}"
+    # echo "Current Directory: $(pwd)"
+    # echo "Directory Stack: [$(dirs)]"
+    # echo "*****"
 
-	for entry in ${folder}/*; do
-		file="$(basename ${entry})"
-		echo -ne "${file}: \t"
-		if [[ ${#file} -lt 6 ]]; then
-			echo -ne "\t"
-		fi
+    for entry in ${folder}/*; do
+        file="$(basename ${entry})"
+        echo -ne "${file}: \t"
+        if [[ ${#file} -lt 6 ]]; then
+            echo -ne "\t"
+        fi
 
-		# Backup entries, remove links
-		if [[ -f ".${file}" ]]; then
-			if [[ ! -h ".${file}" ]]; then
-				mv .${file} ${backupdir}
-				echo -ne "[Moved]\t"
-			fi
-		elif [[ -h ".${file}" && ! -d ".${file}" ]]; then
-			# Here are orphaned symlinks
-			rm .${file}
-			echo -ne "[Orphaned Link Removed]\t"
-		fi
+        # Backup entries, remove links
+        if [[ -f ".${file}" ]]; then
+            if [[ ! -h ".${file}" ]]; then
+                mv .${file} ${backupdir}
+                echo -ne "[Moved]\t"
+            fi
+        elif [[ -h ".${file}" && ! -d ".${file}" ]]; then
+            # Here are orphaned symlinks
+            rm .${file}
+            echo -ne "[Orphaned Link Removed]\t"
+        fi
 
-		# Directories
-		if [[ -d "${entry}" ]]; then
-			if [[ ! -d ".${file}" ]]; then
-				# echo "creating directory: ${dest_dir}/.${file}"
-				mkdir ".${file}"
-				echo "[Directory Created] "
-			else
-				echo "[Directory Exists] "
-			fi
-			pushd ${entry} > /dev/null
-			for dfile in $(find ./ -type f); do
-				echo -n "  $(basename ${dfile}): \t [Linked]"
-				mkdir -p ${dest_dir}/.${file}/$(dirname ${dfile}) &> /dev/null
-				ln -s $(realpath ${dfile}) ${dest_dir}/.${file}/${dfile} &> /dev/null
-			done
-			popd > /dev/null
-		else
-			# Link in configs
-			if [[ ! -h ".${file}" ]]; then
-				ln -s ${entry} .${file}
-				echo -n "[Linked] "
-			else
-				echo -n "[link exists] "
-			fi
-		fi
-		echo " Done!"
-	done
+        # Directories
+        if [[ -d "${entry}" ]]; then
+            if [[ ! -d ".${file}" ]]; then
+                # echo "creating directory: ${dest_dir}/.${file}"
+                mkdir ".${file}"
+                echo "[Directory Created] "
+            else
+                echo "[Directory Exists] "
+            fi
+            pushd ${entry} > /dev/null
+            for dfile in $(find ./ -type f); do
+                echo -n "  $(basename ${dfile}): \t [Linked]"
+                mkdir -p ${dest_dir}/.${file}/$(dirname ${dfile}) &> /dev/null
+                ln -s $(realpath ${dfile}) ${dest_dir}/.${file}/${dfile} &> /dev/null
+            done
+            popd > /dev/null
+        else
+            # Link in configs
+            if [[ ! -h ".${file}" ]]; then
+                ln -s ${entry} .${file}
+                echo -n "[Linked] "
+            else
+                echo -n "[link exists] "
+            fi
+        fi
+        echo " Done!"
+    done
 }
 
 create_links ${dir}/config/
 popd > /dev/null
 
 # Find powerline dependencies
-pvim=$(python3 -c "import powerline as _; print(_.__path__)")
-echo ${pvim}
+#pvim=$(python3 -c "import powerline as _; print(_.__path__)")
+#echo ${pvim}
--- tools/update.sh
+++ tools/update.sh
@@ -1,20 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-cd ~/dotfiles
-git pull --rebase --stat origin master
+cd ${HOME}/dotfiles
 
-if [[ -f ~/.oh-my-zsh/tools/upgrade.sh ]]; then
-	ZSH=/home/nate/.oh-my-zsh
-	env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
+# Install or upgrade Oh My ZSH
+if [[ -f ${HOME}/.oh-my-zsh/tools/upgrade.sh ]]; then
+    ZSH=${HOME}/.oh-my-zsh
+    env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
+else
+    tools/oh-my-zsh/install.sh
 fi
 
-if [[ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
-	pushd ~/.oh-my-zsh/custom/themes/powerlevel9k
-	git pull
-	popd
-elif [[ -d ~/.oh-my-zsh ]]; then
-	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# Install or upgrade powerline-status
+sudo pip3 install --upgrade powerline-status
+
+# Install or upgrade PowerLevel9K
+if [[ -d ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
+    pushd ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
+    git pull
+    popd
+elif [[ -d ${HOME}/.oh-my-zsh ]]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
 fi
 
-~/dotfiles/tools/makesymlinks.sh
-source ~/.zshrc
+# take care of the configuration symlinks
+${HOME}/dotfiles/tools/makesymlinks.sh
+case "zsh" in ("*${SHELL}")
+    source ${HOME}/.zshrc
+esac
