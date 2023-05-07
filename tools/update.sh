e43c9a06b33ce8ab229149bb67be36596a1b6c53
Nathan Thompson <nthompson@thebog.net>
2023-05-07 08:42:25 -0400

    Fix powerline package management

--- tools/update.sh
+++ tools/update.sh
@@ -1,53 +1,5 @@
 #!/bin/bash
 
-cd ${HOME}/dotfiles
-
-# Update ourselves
-git reset --hard > /dev/null
-git pull --rebase --stat origin master
-
-# Install or upgrade Oh My ZSH
-if [[ -f ${HOME}/.oh-my-zsh/tools/upgrade.sh ]]; then
-    ZSH=${HOME}/.oh-my-zsh
-    env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
-else
-    tools/oh-my-zsh/install.sh
-fi
-
-# Install or upgrade powerline-status
-#sudo pip3 install --upgrade powerline-status
-get_os_info()
-if [ ${OS} == "arch" ]; then
-    #PKGMGR=/usr/bin/pacman
-    sudo /usr/bin/pacman -S powerline
-elif [ ${OS} == "debian" || $OS == "ubuntu"]; then
-    #PKGMGR=/usr/bin/apt
-    sudo /usr/bin/apt install powerline
-elif [ ${OS} == "fedora" ]; then
-    #PKGMGR=/usr/bin/dnf
-    sudo /usr/bin/dnf install powerline
-#elif [ ${OS} == "centos" ]; then
-    #PKGMGR=/usr/bin/yum
-#    sudo /usr/bin/yum 
-fi
-
-
-# Install or upgrade PowerLevel9K
-if [[ -d ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
-    pushd ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
-    git pull
-    popd
-elif [[ -d ${HOME}/.oh-my-zsh ]]; then
-    git clone https://github.com/bhilburn/powerlevel9k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
-fi
-
-# take care of the configuration symlinks
-${HOME}/dotfiles/tools/makesymlinks.sh
-case "zsh" in ("*${SHELL}")
-    source ${HOME}/.zshrc
-esac
-
-
 get_os_info() {
     if [ -f /etc/os-release ]; then
         #freedesktop.org and systemd
@@ -78,3 +30,53 @@ get_os_info() {
         VER=$(uname -r)
     fi
 }
+
+cd ${HOME}/dotfiles
+
+# Update ourselves
+#git reset --hard > /dev/null
+#git pull --rebase --stat origin master
+
+# Install or upgrade Oh My ZSH
+if [[ -f ${HOME}/.oh-my-zsh/tools/upgrade.sh ]]; then
+    ZSH=${HOME}/.oh-my-zsh
+    env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
+else
+    tools/oh-my-zsh/install.sh
+fi
+
+# Install or upgrade powerline-status
+#sudo pip3 install --upgrade powerline-status
+get_os_info
+echo "OS: ${OS}"
+echo "VER: ${VER}"
+
+if [ ${OS} == "arch" ]; then
+    #PKGMGR=/usr/bin/pacman
+    sudo /usr/bin/pacman -S --needed powerline
+elif [ ${OS} == "debian" || $OS == "ubuntu"]; then
+    #PKGMGR=/usr/bin/apt
+    sudo /usr/bin/apt install powerline
+elif [ ${OS} == "fedora" ]; then
+    #PKGMGR=/usr/bin/dnf
+    sudo /usr/bin/dnf install powerline
+#elif [ ${OS} == "centos" ]; then
+    #PKGMGR=/usr/bin/yum
+#    sudo /usr/bin/yum 
+fi
+
+
+# Install or upgrade PowerLevel9K
+if [[ -d ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
+    pushd ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
+    git pull
+    popd
+elif [[ -d ${HOME}/.oh-my-zsh ]]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k
+fi
+
+# take care of the configuration symlinks
+${HOME}/dotfiles/tools/makesymlinks.sh
+case "zsh" in ("*${SHELL}")
+    source ${HOME}/.zshrc
+esac
