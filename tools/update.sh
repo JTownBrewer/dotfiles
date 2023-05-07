50e4bb01910c914160a9682744b09e5264e77cef
Nathan Thompson <nthompson@thebog.net>
2023-05-06 23:42:42 -0400

    Stop installing powerline via pip. Use the system's package manager, instead

--- tools/update.sh
+++ tools/update.sh
@@ -15,7 +15,22 @@ else
 fi
 
 # Install or upgrade powerline-status
-sudo pip3 install --upgrade powerline-status
+#sudo pip3 install --upgrade powerline-status
+get_os_info()
+if [ ${OS} == "arch" ]; then
+    #PKGMGR=/usr/bin/pacman
+    sudo /usr/bin/pacman -S powerline
+elif [ ${OS} == "debian" || $OS == "ubuntu"]; then
+    #PKGMGR=/usr/bin/apt
+    sudo /usr/bin/apt install powerline
+elif [ ${OS} == "fedora" ]; then
+    #PKGMGR=/usr/bin/dnf
+    sudo /usr/bin/dnf install powerline
+#elif [ ${OS} == "centos" ]; then
+    #PKGMGR=/usr/bin/yum
+#    sudo /usr/bin/yum 
+fi
+
 
 # Install or upgrade PowerLevel9K
 if [[ -d ${HOME}/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
@@ -31,3 +46,35 @@ ${HOME}/dotfiles/tools/makesymlinks.sh
 case "zsh" in ("*${SHELL}")
     source ${HOME}/.zshrc
 esac
+
+
+get_os_info() {
+    if [ -f /etc/os-release ]; then
+        #freedesktop.org and systemd
+        . /etc/os-release
+        OS=$ID
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        OS=$DISBRIB_ID
+        VER=$DISRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+#    elif [ -f /etc/SuSe-release ]; then
+        # Older SuSE/etc.
+    ...
+#    elif [ -f /etc/redhat-release ]; then
+        # Older Red Hat, CentOS, etc.
+    ...
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
+}
